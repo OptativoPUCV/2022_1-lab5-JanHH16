3a9311fa233104ee6c6fe7e81908885e9349eb8a
Your Name <you@example.com>
2022-04-23 15:37:24 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -312,6 +312,7 @@ Pair * nextTreeMap(TreeMap * tree)
             {
                 nodoAux = nodoAux->parent;
                 tree->current = nodoAux;
+                printf("%d", *(int*) nodoAux->pair->key)
                 return nodoAux->pair;
             }
                    
